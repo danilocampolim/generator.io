4ba455688be7eff6cf6668a2a2530724c9495f13
danilocampolim <danilo.campolim@gmail.com>
2026-05-14 19:36:14 -0400

    Add files via upload

--- houses.js
+++ houses.js
@@ -1,4 +1,14 @@
 const houses = [
-  { resort: "Crystal Cove", name: "1067 Tourmaline Dr. (4bd)", price: 120 },
-  { resort: "Crystal Cove", name: "1069 Tourmaline Dr. (4bd)", price: 120 }
+  { resort: "Storey Lake", name: "4299 Paragraph Dr. (5bd)", price: 100 },
+  { resort: "Storey Lake", name: "4514 Narrative Dr. (7bd)", price: 150 },
+  { resort: "Windsor Cay", name: "1145 Blue Island Dr. (5bd)", price: 100 },
+  { resort: "Storey Lake", name: "4729 Kings Castle Cir. (7bd)", price: 150 }, 
+  { resort: "Storey Lake", name: "4875 Romeo Cir. (4bd)", price: 85 },  
+  { resort: "Storey Lake", name: "3117 Pequod Pl. (5bd)", price: 85 },
+  { resort: "Storey Lake", name: "3151 Paradox Cl. #107 (4bd)", price: 50 },
+  { resort: "Storey Lake", name: "4819 Romeo Cir. (4bd)", price: 85 },  
+  { resort: "SummerVille", name: "2650 Sunrise Shores Dr. (5bd)", price: 100 },  
+  { resort: "Storey Lake", name: "2600 Reading Trl. (4bd)", price: 85 },
+  { resort: "Storey Lake", name: "2649 Scrapbook Ct. (4bd)", price: 85 },
+  { resort: "Storey Lake", name: "4809 Dedication St. (4bd)", price: 85 }   
 ];
