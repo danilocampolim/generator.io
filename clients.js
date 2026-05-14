8172e23aec11373b2fd7d12938353ad1f2b1a61b
danilocampolim <danilo.campolim@gmail.com>
2026-05-14 19:14:03 -0400

    Add files via upload

--- clients.js
+++ clients.js
@@ -1,4 +1,4 @@
 const clients = [
-  { name: "Greengol Property Management LLC", address: "9852 Emerald Berry Dr, Winter Garden, FL, 34787", phone: "(407) 724-5516" },
+  { name: "Glasstone Group Shared Services LLC", address: "4700 Millenia Boulevard, Suite 360, Orlando, FL, 32839", phone: "(321) 323-1816" },
   { name: "Greengol Property Management LLC", address: "9852 Emerald Berry Dr, Winter Garden, FL, 34787", phone: "(407) 724-5516" }
 ];
