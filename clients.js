fac1bbd75a05cece88803b1fcbc89373227780ff
danilocampolim <danilo.campolim@gmail.com>
2026-05-14 18:57:31 -0400

    Add files via upload

--- clients.js
+++ clients.js
@@ -1,3 +1,4 @@
 const clients = [
+  { name: "Greengol Property Management LLC", address: "9852 Emerald Berry Dr, Winter Garden, FL, 34787", phone: "(407) 724-5516" },
   { name: "Greengol Property Management LLC", address: "9852 Emerald Berry Dr, Winter Garden, FL, 34787", phone: "(407) 724-5516" }
 ];
